ad4a1292dbf1d6817439aa75e7be56011c6f17a3
Skilvzlom <fedyakur1@gmail.com>
2024-10-17 20:05:25 +0300

    final field

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    private CarServiceImp carService;
+    private final CarServiceImp carService;
 
     @Autowired
     public CarController(CarServiceImp carService) {
